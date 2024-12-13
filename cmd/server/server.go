3b1648c7200fb92cf25e18229a95ecbd34d4ee98
manueldembo <madembo18@gmail.com>
2024-12-13 15:27:41 +0100

    feat: save exchange rate on db

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -2,52 +2,85 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"encoding/json"
+	"errors"
+	"log"
 	"net/http"
 	"time"
+
+	"github.com/google/uuid"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	http.HandleFunc("/cotacao", ExchangeRateHandler)
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatalf("Failed to open database: %v", err)
+	}
+	defer db.Close()
+
+	http.HandleFunc("/cotacao", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Accept", "application/json")
+
+		e, err := getExchangeRate()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		err = saveExchangeRate(db, e)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		json.NewEncoder(w).Encode(e)
+	})
 	http.ListenAndServe(":8080", nil)
 }
 
-func ExchangeRateHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Accept", "application/json")
-
-	e, err := getExchangeRate()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	json.NewEncoder(w).Encode(e)
-}
-
-func getExchangeRate() (*string, error) {
+func getExchangeRate() (string, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*200)
 	defer cancel()
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "https://economia.awesomeapi.com.br/json/last/USD-BRL", nil)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	defer res.Body.Close()
 
-	var exchangeRate ExchangeRate
+	var exchangeRate ExchangeRateDTO
 	err = json.NewDecoder(res.Body).Decode(&exchangeRate)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
-	return &exchangeRate.USDBRL.Bid, nil
+	return exchangeRate.USDBRL.Bid, nil
 }
 
-type ExchangeRate struct {
+func saveExchangeRate(db *sql.DB, value string) error {
+	if value == "" {
+		return errors.New("invalid exchange rate")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*10)
+	defer cancel()
+
+	id := uuid.New().String()
+	_, err := db.ExecContext(ctx, "INSERT INTO exchange_rates (id, value) VALUES ($1, $2)", id, value)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type ExchangeRateDTO struct {
 	USDBRL struct {
 		Bid string `json:"bid"`
 	} `json:"USDBRL"`
